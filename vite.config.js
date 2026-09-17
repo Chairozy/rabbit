841fb661da1ef4de216485134f9160e761227e61
Chairozy <chairozyrozy@gmail.com>
2026-09-17 10:52:31 +0700

    Samakan base path dengan nama repo rabbit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/rabbit-overworld/',
+  base: '/rabbit/',
   plugins: [tailwindcss(), svelte()]
 });
